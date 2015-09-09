6cbe6529146c32f1f81d656e2a2a4dc2e1148c50
Will Speak <lithiumflame@gmail.com>
2015-09-09 08:00:02 +0100

    Update Build to Use Paths
    Move to using paths rather than strings for the file locations in the
    build script. Also add a snooper config file for running the build and
    test continuously.

--- build.rs
+++ build.rs
@@ -1,30 +1,46 @@
 use std::env;
 use std::process::Command;
+use std::path::{Path, PathBuf};
 use std::fs;
+use std::fs::metadata;
+
+fn check_exists(file: &PathBuf) -> bool {
+    if let Ok(m) = metadata(file) {
+        m.is_file()
+    } else {
+        false
+    }
+}
 
 pub fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    Command::new("tar")
-        .arg("zxf")
-        .arg("onig-5.9.6.tar.gz")
-        .status().unwrap_or_else(|err| {
-            panic!("Error extracting onig tar file: {}", err);
-        });
-    env::set_current_dir("onig-5.9.6").unwrap();
-    Command::new("./configure")
-        .status().unwrap_or_else(|err| {
-            panic!("Error running configure: {}", err);
-        });
-    Command::new("make")
-        .status().unwrap_or_else(|err| {
-            panic!("Error running make: {}", err);
-        });
-    fs::copy(".libs/libonig.dylib", format!("{}/libonig.dylib", &out_dir))
-        .unwrap_or_else(|err| {
-            panic!("Error copying file to output: {}", err);
-        });
+    let out_dir_str = env::var("OUT_DIR").unwrap();
+    let out_dir = Path::new(&out_dir_str);
+    let out_file = out_dir.join("libonig.dylib");
 
-    println!("cargo:rustc-link-search=native={}", out_dir);
+    // If the file already exists then skip compiling it
+    if !check_exists(&out_file) {
+        Command::new("tar")
+            .arg("zxf")
+            .arg("onig-5.9.6.tar.gz")
+            .status().unwrap_or_else(|err| {
+                panic!("Error extracting onig tar file: {}", err);
+            });
+        env::set_current_dir("onig-5.9.6").unwrap();
+        Command::new("./configure")
+            .status().unwrap_or_else(|err| {
+                panic!("Error running configure: {}", err);
+            });
+        Command::new("make")
+            .status().unwrap_or_else(|err| {
+                panic!("Error running make: {}", err);
+            });
+        fs::copy(".libs/libonig.dylib", out_file)
+            .unwrap_or_else(|err| {
+                panic!("Error copying file to output: {}", err);
+            });
+    }
+
+    println!("cargo:rustc-link-search=native={}", out_dir.to_str().unwrap());
     println!("cargo:rustc-link-lib=dylib=onig");
 
 }
