dc8e69901447ec312c672ff3d5b8fec0f17282b2
Will Speak <lithiumflame@gmail.com>
2015-09-12 20:06:53 +0100

    Add Bindings for Regex Creation
    Adds the funciton definitions for creating a regex and a new wrapper
    struct `Regex` for creating and managing them from Rust, including an
    implementation of the `Drop` trait to automatically free the `regex_t`
    pointers.
    
    This also adds an `Error` struct with an implementaiton for the `Debug`
    trait which prints out the formatted error string from Oniguruma.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,38 @@
 extern crate libc;
 
 use libc::c_int;
+use std::ptr::null;
+use std::slice;
+use std::str;
+use std::fmt;
 
 mod onig_sys;
 mod utils;
+
+pub struct Error {
+    error: libc::c_int,
+    error_info: Option<onig_sys::OnigErrorInfo>
+}
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut err_buff = &mut [0 as u8; 1024];
+        let len  = unsafe {
+            match self.error_info {
+                Some(ref error_info) =>
+                    onig_sys::onig_error_code_to_str(
+                        err_buff.as_mut_ptr(),
+                        self.error,
+                        error_info as *const onig_sys::OnigErrorInfo),
+                None =>
+                    onig_sys::onig_error_code_to_str(
+                        err_buff.as_mut_ptr(), self.error)
+            }
+        };
+        let err_str_slice = str::from_utf8(&err_buff[..len as usize]).unwrap();
+        write!(f, "Oniguruma error: {}", err_str_slice)
+    }
+}
 
 #[allow(raw_pointer_derive)]
 #[derive(Debug)]
@@ -40,12 +69,65 @@ impl Drop for OnigRegion {
     }
 }
 
+#[allow(raw_pointer_derive)]
+#[derive(Debug)]
+pub struct Regex {
+    raw: *const onig_sys::regex_t
+}
 
+impl Regex {
+    fn new(
+        pattern: &str,
+        option: onig_sys::OnigOptionType,
+        syntax: *const onig_sys::OnigSyntaxTypeStruct
+            ) -> Result<Regex, Error> {
+
+        // Convert the rust types to those required for the call to
+        // `onig_new`.
+        let pattern_bytes = pattern.as_bytes();
+        let mut reg: *const onig_sys::regex_t = null();
+        let mut reg_ptr = &mut reg as *mut *const onig_sys::regex_t;
+
+        // We can use this later to get an error message to pass back
+        // if regex creation fails.
+        let mut error = onig_sys::OnigErrorInfo {
+            enc: null(),
+            par: null(),
+            par_end: null()
+        };
+
+        let err = unsafe {
+            onig_sys::onig_new(
+                reg_ptr,
+                pattern_bytes.as_ptr(),
+                pattern_bytes[pattern_bytes.len()..].as_ptr(),
+                option,
+                onig_sys::OnigEncoding::UTF8,
+                syntax,
+                &mut error)
+        };
+
+        if err == 0 {
+            Ok(Regex{ raw: reg })
+        } else { 
+            Err(Error{ error: err, error_info: Some(error) })
+        }
+    }
+}
+
+impl Drop for Regex {
+    fn drop(&mut self) {
+        unsafe {
+            onig_sys::onig_free(self.raw);
+        }
+    }
+}
 
 #[cfg(test)]
 mod test_lib {
 
     use super::*;
+    use super::onig_sys;
 
     #[test]
     fn test_region_create() {
@@ -56,5 +138,21 @@ mod test_lib {
     fn test_region_clear() {
         let mut region = OnigRegion::new();
         region.clear();
+    }
+
+    #[test]
+    fn test_regex_create() {
+        Regex::new(
+            ".*", onig_sys::OnigOptionType::NONE,
+            onig_sys::OnigSyntaxType::RUBY).unwrap();
+    }
+    
+    #[test]
+    #[should_panic(expected = "Oniguruma error: invalid character property name {foo}")]
+    fn test_regex_invalid() {
+        Regex::new(
+            "\\p{foo}",
+            onig_sys::OnigOptionType::NONE,
+            onig_sys::OnigSyntaxType::RUBY).unwrap();
     }
 }
