26286f007369422594ec7803600cba2add1d971c
Will Speak <lithiumflame@gmail.com>
2016-02-12 07:56:27 +0000

    Add Git Submodule and Move to CMake for Build
    This updates the build process to track defuz/oniguruma and switches to
    building oniguruma with CMake. This should pave the way to building on
    Windows.
    
    There are still some problems with dynamic linker paths and the
    libraries produced by CMake.

--- onig_sys/build.rs
+++ onig_sys/build.rs
@@ -1,54 +1,17 @@
 extern crate pkg_config;
+extern crate cmake;
 
-use std::env;
-use std::process::Command;
-use std::path::Path;
-use std::fs;
+fn compile_with_cmake() {
+    use cmake::Config;
 
-#[cfg(target_os = "macos")]
-static LIB_NAME: &'static str = "libonig.dylib";
-#[cfg(not(target_os = "macos"))]
-static LIB_NAME: &'static str = "libonig.so";
+    // Builds the project in the directory located in `oniguruma`, installing it
+    // into $OUT_DIR
+    let dst = Config::new("oniguruma")
+        .define("CMAKE_MACOSX_RPATH", "NO")
+        .build();
 
-fn compile_with_make() {
-    let out_dir_str = env!("OUT_DIR");
-    let onig_tar_out_dir = Path::new("./onig-5.9.6/");
-    let lib_dir = Path::new(out_dir_str).join("lib");
-    let out_file = lib_dir.join(LIB_NAME);
-
-    // If the file already exists then skip compiling it
-    if !out_file.exists() {
-        if onig_tar_out_dir.exists() {
-            fs::remove_dir_all(onig_tar_out_dir).unwrap_or_else(|err| {
-                panic!("Could not remove tar output directory: {}", err);
-            });
-        }
-        Command::new("tar")
-            .arg("zxf")
-            .arg("onig-5.9.6.tar.gz")
-            .status().unwrap_or_else(|err| {
-                panic!("Error extracting onig tar file: {}", err);
-            });
-        env::set_current_dir(onig_tar_out_dir).unwrap();
-        Command::new("./configure")
-            .arg(format!("--prefix={}", out_dir_str))
-            .status().unwrap_or_else(|err| {
-                panic!("Error running configure: {}", err);
-            });
-        Command::new("make")
-            .status().unwrap_or_else(|err| {
-                panic!("Error running make: {}", err);
-            });
-        Command::new("make")
-            .arg("install")
-            .status().unwrap_or_else(|err| {
-                panic!("Error running make install: {}", err);
-            });
-    }
-
-    println!("cargo:rustc-link-search=native={}", lib_dir.display());
+    println!("cargo:rustc-link-search=native={}", dst.join("build").display());
     println!("cargo:rustc-link-lib=dylib=onig");
-
 }
 
 pub fn main() {
@@ -56,5 +19,5 @@ pub fn main() {
         return;
     }
 
-    compile_with_make();
+    compile_with_cmake();
 }
