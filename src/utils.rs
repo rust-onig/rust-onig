01576cc728abea5490213724859ddfb653b16bdb
Will Speak <lithiumflame@gmail.com>
2018-03-17 11:15:22 +0000

    Upgrade to Oniguruma 6.8.0
    Initial work on #75.

--- src/utils.rs
+++ src/utils.rs
@@ -51,7 +51,7 @@ mod tests {
     #[test]
     pub fn utils_get_version_returns_expected_version() {
         let version = version();
-        assert_eq!(version, "6.7.1");
+        assert_eq!(version, "6.8.0");
     }
 
     #[test]
