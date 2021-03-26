5bc2646f6cfc817d6b81de484eddec7bb171c052
Oron Sharabi <oronsh16@gmail.com>
2021-03-26 17:38:43 +0300

    Add set_syntax_op fn

--- onig/src/syntax.rs
+++ onig/src/syntax.rs
@@ -107,6 +107,38 @@ impl Syntax {
         }
     }
 
+    /// Get main syntax operators
+    pub fn get_syntax_op(&self) -> SyntaxOperator {
+	unsafe {
+            let op = onig_sys::onig_get_syntax_op(self.raw_mut());
+            SyntaxOperator::from_bits_truncate(u64::from(op))	
+	}
+    }
+
+    /// Set main syntax operators
+    pub fn set_syntax_op(&mut self, operators: SyntaxOperator) {
+	unsafe {
+	    let op = onig_sys::onig_get_syntax_op(self.raw_mut());
+	    onig_sys::onig_set_syntax_op(&mut self.raw, op + (operators.bits() as onig_sys::OnigSyntaxOp));
+	}
+    }
+
+    /// Get secondary syntax operators
+    pub fn get_syntax_op2(&self) -> SyntaxOperator {
+	unsafe {
+            let op = onig_sys::onig_get_syntax_op2(self.raw_mut());
+            SyntaxOperator::from_bits_truncate(u64::from(op) << 32)
+	}
+    }
+
+    /// Set secondary syntax operators    
+    pub fn set_syntax_op2(&mut self, operators: SyntaxOperator) {
+	unsafe {
+	    let op = onig_sys::onig_get_syntax_op2(self.raw_mut());
+	    onig_sys::onig_set_syntax_op2(&mut self.raw, op + (operators.bits() as onig_sys::OnigSyntaxOp));
+	}
+    }
+    
     /// Replace the operators for this syntax
     pub fn set_operators(&mut self, operators: SyntaxOperator) {
         let op = operators.bits() as onig_sys::OnigSyntaxOp;
